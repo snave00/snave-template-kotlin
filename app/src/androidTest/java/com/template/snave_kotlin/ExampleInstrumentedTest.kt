26e0aad00c29eec8362fdfe09e776f864c97835f
snave <evansonmanums@gmail.com>
2021-05-16 18:26:27 +0800

    Updated Package name on Unit test

--- app/src/androidTest/java/com/template/snave_kotlin/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/template/snave_kotlin/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.evansontemplate", appContext.packageName)
+        assertEquals("com.template.snave_kotlin", appContext.packageName)
     }
 }
